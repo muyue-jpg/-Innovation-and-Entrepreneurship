83743686b50621096e6c4631fc7c8ed5140c4f52
hungry <66394822+muyue-jpg@users.noreply.github.com>
2022-07-23 13:17:59 +0800

    Add files via upload

--- sm3rho/sm3_RHO.py
+++ sm3rho/sm3_RHO.py
@@ -2,8 +2,9 @@ import sm3
 import random
 import time
 
+
 def hash(exm):  # 获取哈希值
-    a = hex(random.randint(0, 2 ** (exm + 1) - 1))[2:]
+    a = hex(random.randint(0, pow(2, (exm+1)) - 1))[2:]
     a_1 = sm3.G_hash(a)
     a_2 = sm3.G_hash(a_1)
     return a, a_1, a_2
@@ -32,8 +33,7 @@ if __name__ == '__main__':
     r, r_1, r_2 = hash(example)
     x, m1, m2 = rho_method(int(example / 4), r, r_1, r_2)
     end = time.time()
-    time=end-start
-    print("success！")
-    print("message1:", m1)
-    print("message2:", m2)
-    print("两者哈希值的前{}bit相同，16进制表示为:{},时间为：{}s".format(example, x, time))
+    time = end - start
+    print("攻击成功")
+    print("message1:", m1, "message2:", m2)
+    print("两者hash值的前", example, "比特相同", "16进制表示:", x, "time:", time)
