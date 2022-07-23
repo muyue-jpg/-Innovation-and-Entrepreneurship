2c0f7f39464ba6039e680a0c8af5a0f1233ac9ac
hungry <66394822+muyue-jpg@users.noreply.github.com>
2022-07-23 13:17:38 +0800

    Add files via upload

--- sm3birth/sm3_birth.py
+++ sm3birth/sm3_birth.py
@@ -1,9 +1,10 @@
 import sm3
 import time
 
+
 def birth_atk(exm):
-    Primal_space = int(2 ** (exm / 2))  # 搜索空间大小
-    ans = [0] * 2**exm
+    Primal_space = int(pow(2, exm / 2))  # 搜索空间大小
+    ans = [0] * pow(2, exm)
     for i in range(Primal_space):
         temp = int(sm3.G_hash(str(i))[0:int(exm / 4)], 16)
         if ans[temp] == 0:
@@ -13,10 +14,11 @@ def birth_atk(exm):
 
 
 if __name__ == '__main__':
-
     example = 24
-    start=time.time()
+    start = time.time()
     a, m_1, m_2 = birth_atk(example)
-    end=time.time()
-    time=end-start
-    print("success! 找到碰撞，消息{}与{}哈希值的前{}bit相同，16进制表示为:{},所花费时间为:{} 。".format(m_1, m_2, example, a, time))
+    end = time.time()
+    time = end - start
+    print("碰撞成功")
+    print("消息:", m_1, "和消息:", m_2, "的hash值的前", example, "比特相同")
+    print("16进制表示:", a, "time:", time)
