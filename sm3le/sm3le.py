a5d23fd6b8cff49cf867e7fe506eb004cbf537ab
hungry <66394822+muyue-jpg@users.noreply.github.com>
2022-07-27 10:01:19 +0800

    Add files via upload

--- sm3le/sm3le.py
+++ sm3le/sm3le.py
@@ -1,49 +1,7 @@
-
-"""
-@Description：SM3长度扩展攻击
-    1.随机生成一个secret，算出secret的hash值
-    2.根据hash值推出第一次压缩之后各个寄存器里的值
-    3.在secret+padding之后附加一段消息，用上一步寄存器里的值作为IV去加密附加的那段消息，得到hash
-    4.用sm3去加密secret+padding+m'，得到hash
-    5.第3步和第4步得到的hash值应该相等
-"""
 from gmssl import sm3, func
 import random
 import my_sm3
 import struct
-
-secret = str(random.random())
-secret_hash = sm3.sm3_hash(func.bytes_to_list(bytes(secret, encoding='utf-8')))
-secret_len = len(secret)
-append_m = "4664"   # 附加消息
-pad_str = ""
-pad = []
-
-def generate_guess_hash(old_hash, secret_len, append_m):
-    """
-    SM3长度扩展攻击
-    :param old_hash: secret的hash值
-    :param secret_len: secret的长度
-    :param append_m: 附加的消息
-    :return: hash(secret + padding + append_m)
-    """
-    vectors = []
-    message = ""
-    # 将old_hash分组，每组8个字节, 并转换为整数
-    for r in range(0, len(old_hash), 8):
-        vectors.append(int(old_hash[r:r + 8], 16))
-
-    # 伪造消息
-    if secret_len > 64:
-        for i in range(0, int(secret_len / 64) * 64):
-            message += 'a'
-    for i in range(0, secret_len % 64):
-        message += 'a'
-    message = func.bytes_to_list(bytes(message, encoding='utf-8'))
-    message = padding(message)
-    message.extend(func.bytes_to_list(bytes(append_m, encoding='utf-8')))
-    return my_sm3.sm3_hash(message, vectors)
-
 
 def padding(msg):
     mlen = len(msg)
@@ -65,28 +23,57 @@ def padding(msg):
     return msg
 
 
-guess_hash = generate_guess_hash(secret_hash, secret_len, append_m)
-new_msg = func.bytes_to_list(bytes(secret, encoding='utf-8'))
-new_msg.extend(pad)
-new_msg.extend(func.bytes_to_list(bytes(append_m, encoding='utf-8')))
-new_msg_str = secret + pad_str + append_m
+def hash(old_hash, secret_len, append_m):
+    vectors = []
+    message = ""
+    # 将old_hash分组，每组8个字节, 并转换为整数
+    for r in range(0, len(old_hash), 8):
+        vectors.append(int(old_hash[r:r + 8], 16))
 
-new_hash = sm3.sm3_hash(new_msg)
+    # 伪造消息
+    if secret_len > 64:
+        for i in range(0, int(secret_len / 64) * 64):
+            message += '0'
+    for i in range(0, secret_len % 64):
+        message += '0'
+    message = func.bytes_to_list(bytes(message, encoding='utf-8'))
+    message = padding(message)
+    message.extend(func.bytes_to_list(bytes(append_m, encoding='utf-8')))
+    return my_sm3.sm3_hash(message, vectors)
 
-print("生成secrect")
-print("secret: "+secret)
-print("secret length:%d" % len(secret))
-print("secret hash:" + secret_hash)
-print("附加消息:", append_m)
-print("-----------------------------------------------------")
-print("计算人为构造的消息的hash值")
-print("hash_guess:" + guess_hash)
-print("-----------------------------------------------------")
-print("验证攻击是否成功")
-print("计算hash(secret+padding+m')")
-print("new message: \n" + new_msg_str)
-print("hash(new message):" + new_hash)
-if new_hash == guess_hash:
-    print("success!")
-else:
-    print("fail..")
+
+
+
+def attack(n1, n2, n3):  # attack(secret, append_m, secret_hash)
+    guess_hash = hash(n3, len(n1), n2)
+    new_msg = func.bytes_to_list(bytes(n1, encoding='utf-8'))
+    new_msg.extend(pad)
+    new_msg.extend(func.bytes_to_list(bytes(n2, encoding='utf-8')))
+    new_msg_str = n1 + pad_str + n2
+    new_hash = sm3.sm3_hash(new_msg)
+    return guess_hash, new_msg_str, new_hash
+
+
+if __name__ == '__main__':
+    secret = str(random.random())
+    secret_hash = sm3.sm3_hash(func.bytes_to_list(bytes(secret, encoding='utf-8')))
+    append_m = input("请输入：")  # 附加消息
+    pad_str = ""
+    pad = []
+
+    guess_hash, new_msg_str, new_hash = attack(secret, append_m, secret_hash)
+    print("生成secrect")
+    print("secret: " + secret)
+    print("secret length:%d" % len(secret))
+    print("secret hash:" + secret_hash)
+    print("附加消息:", append_m)
+    print("人为构造的消息的hash值")
+    print("hash_guess:" + guess_hash)
+    print("验证攻击是否成功")
+    print("计算hash(secret+padding+append_m')")
+    print("new message: \n" + new_msg_str)
+    print("hash(new message):" + new_hash)
+    if new_hash == guess_hash:
+        print("success!")
+    else:
+        print("fail..")
